1417813a5f366836f8f26d4de9ef233361d7d0a6
Mlbrn <114309689+mlbrnm@users.noreply.github.com>
2024-08-01 15:38:46 -0600

    Added the Node / SLL files.

--- Assignment3.Tests/obj/Debug/net6.0/Assignment3.Tests.AssemblyInfo.cs
+++ Assignment3.Tests/obj/Debug/net6.0/Assignment3.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+984d354d2305a67744f44af8d295ec2eddcf8f96")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Assignment3/Utility/SLL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assignment3.Utility
+{
+	public class SLL : ILinkedListADT
+	{
+		public Node Head { get; set; }
+		public Node Tail { get; set; }
+		public int Count { get; set; }
+		public bool IsEmpty()
+		{
+			if (Head == null)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	
+}
